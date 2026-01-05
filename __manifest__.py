b7cc967b1e3381f67bc6c9cf7522c5327bd5f861
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2026-01-05 11:29:14 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Construction',
     'summary': 'Construction BOQ Management',
     'author': 'ELB Marketing',
-    'website': 'https://www.entrpryz.com',
+    'website': 'https://entrpryz.com',
     'depends': ['base', 'project', 'analytic', 'purchase', 'stock', 'account'],
     'data': [
         'security/ir.model.access.csv',
